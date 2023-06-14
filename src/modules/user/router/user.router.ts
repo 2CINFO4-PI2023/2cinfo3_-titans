b5b1f347540cbbf4d4047417270edaea976a2870
mohamed sallami rhouma <mohamed-sr@hotmail.com>
2023-06-14 23:08:04 +0100

    complete user crud endpoints

--- src/modules/user/router/user.router.ts
+++ src/modules/user/router/user.router.ts
@@ -10,8 +10,24 @@ export class UserRouter {
     return this._userRoutes;
   }
   private init() {
-    this._userRoutes.route("").post((req,res)=>{
-      this.userController.create(req,res)
-    });
+    this._userRoutes
+      .route("")
+      .post((req, res) => {
+        this.userController.create(req, res);
+      })
+      .get((req, res) => {
+        this.userController.getAll(req, res);
+      });
+    this._userRoutes
+      .route("/:id")
+      .get((req, res) => {
+        this.userController.get(req, res);
+      })
+      .delete((req, res) => {
+        this.userController.delete(req, res);
+      })
+      .put((req, res) => {
+        this.userController.update(req, res);
+      });
   }
 }
