f5ea64b3c256d97d55ab39e1a7298e964fad0685
Hamza Taoujouti <hamza.taoujouti@sofrecom.com>
2023-06-21 10:19:02 +0100

    use hashmap in plat for more efficiancy

--- src/modules/stock/model/plat.schema.ts
+++ src/modules/stock/model/plat.schema.ts
@@ -3,17 +3,18 @@ import { IIngredient, Ingredient } from "./ingredient.schema";
 
 export interface IPlat extends Document {
     name: string;
-    ingredients: Array<{ ingredient: IIngredient['_id']; quantity: number }>;
+    ingredients: Map< IIngredient['_id'], number>;
     price: number;
     image: string;
 }
 
 const platSchema = new Schema<IPlat>({
     name: { type: String, required: true },
-    ingredients: [{
-        ingredient: { type: Schema.Types.ObjectId, ref: Ingredient },
-        quantity: { type: Number, required: true }
-    }],
+    ingredients: {
+        type: Map,
+        of: Number,
+        required: true,
+      },
     price: { type: Number, required: true },
     image: { type: String, required: true }
 
