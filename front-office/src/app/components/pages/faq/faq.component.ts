4de5e3a0d246e3eba9e159fb321276869c4e8637
CHTSaif <101019876+CHTSaif@users.noreply.github.com>
2023-07-16 17:37:53 +0200

    update

--- front-office/src/app/components/pages/faq/faq.component.ts
+++ front-office/src/app/components/pages/faq/faq.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ReclamationService } from './faq.service';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.sass']
 })
-export class FaqComponent implements OnInit {
+export class FaqComponent {
+  claimCreated: boolean = false;
+  claimDescription: string = '';
+  reclamationType: string = '';
+  id: string = ''; // Add the 'id' property and set its appropriate value
 
-  constructor() { }
+  constructor(private reclamationService: ReclamationService) {}
 
-  ngOnInit() {
+  generateRandomNumber(): string {
+    const prefix = 'RC-';
+    const randomNumber = Math.floor(1000 + Math.random() * 9000);
+    return `${prefix}${randomNumber}`;
   }
 
+  ngOnInit() {
+    this.reclamationService.getNewStatusId().subscribe(
+      (response) => {
+        this.id = response._id;
+      },
+      (error) => {
+        console.error('Failed to get new statut!', error);
+      }
+    );
+  }
+
+  submitClaim(claimDescription: string, reclamationType: string) {
+    const reclamation = {
+      description: claimDescription,
+      type: reclamationType,
+      statut: this.id,
+      numero: this.generateRandomNumber()
+    };
+
+    this.reclamationService.createReclamation(reclamation).subscribe(
+      (response) => {
+        console.log('Claim submitted successfully.', response);
+        this.claimCreated = true;
+        this.claimDescription = '';
+        this.reclamationType = '';
+        alert('Claim created!');
+      },
+      (error) => {
+        console.error('Failed to submit claim.', error);
+      }
+    );
+  }
 }
